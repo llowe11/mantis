38c446a2961740f1f8427ea61c03d5da39376614
llowe11 <vladislavkim566@gmail.com>
2025-01-30 16:36:13 +0500

    FEAT: app delegator

--- mantis/SecondScreen.swift
+++ mantis/SecondScreen.swift
@@ -9,15 +9,4 @@ class SecondScreen: UIViewController {
         view.backgroundColor = .systemPink
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
