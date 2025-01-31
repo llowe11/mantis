9ad1dabc92c6713bf90260650a41697740b63407
llowe11 <vladislavkim566@gmail.com>
2025-01-31 09:41:33 +0500

    FEAT: change second screen

--- mantis/SecondScreen.swift
+++ mantis/SecondScreen.swift
@@ -2,11 +2,15 @@
 import UIKit
 
 class SecondScreen: UIViewController {
-
+    let screenButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        
+        setupButton()
     }
     
+    
+    func setupButton(){
+        view.addSubview(screenButton)
+    }
 }
