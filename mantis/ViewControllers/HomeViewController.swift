995d79ea338e40e14d813318be1766afd58aad85
llowe11 <vladislavkim566@gmail.com>
2025-02-06 19:32:34 +0500

    FEAT: position elements on the screen

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -7,6 +7,13 @@ class HomeViewController: UIViewController {
         label.textColor = .black
         return label
     }()
+    
+    private var homeButton:UIButton = {
+        let hButton = UIButton()
+        hButton.setTitle("click", for: .normal)
+        hButton.backgroundColor = .white
+        return hButton
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
@@ -14,9 +21,15 @@ class HomeViewController: UIViewController {
         view.addSubview(secondaryLabel)
         secondaryLabel.translatesAutoresizingMaskIntoConstraints = false
         
+        view.addSubview(homeButton)
+        homeButton.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             secondaryLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            secondaryLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            secondaryLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            homeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            homeButton.topAnchor.constraint(equalTo: secondaryLabel.bottomAnchor, constant: 20),
         ])
     }
     
