aa3113e61d49e96ed69825a9ab881ce266fd5ea2
llowe11 <vladislavkim566@gmail.com>
2025-02-06 19:16:24 +0500

    FEAT: edit ui parts

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -1,11 +1,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private var textLabel = UILabel()
+    private var secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "some random text"
+        label.textColor = .black
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        
+        view.addSubview(secondaryLabel)
+        secondaryLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            secondaryLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondaryLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
 }
