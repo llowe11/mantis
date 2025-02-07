072c307258c96a19a9c122fac3a524c72c8bcd3d
llowe11 <vladislavkim566@gmail.com>
2025-02-07 20:31:53 +0500

    FEAT: create scroll view

--- mantis/SceneDelegate.swift
+++ mantis/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = StacksViewController()
+        window?.rootViewController = ScrollViewController()
         window?.makeKeyAndVisible()
     }
 
--- mantis/ViewControllers/ScrollViewController.swift
+++ mantis/ViewControllers/ScrollViewController.swift
@@ -1,29 +1,56 @@
-//
-//  ScrollViewController.swift
-//  mantis
-//
-//  Created by Vladislav Kim on 07.02.2025.
-//
-
 import UIKit
 
 class ScrollViewController: UIViewController {
 
+    private var scrollView = UIScrollView()
+    private var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 20
+        return stack
+    }()
+    
+    private let rectHeight: CGFloat = 200
+    private let numOfBlocks = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+        for _ in 1..<numOfBlocks{
+            let rect = createRectangle()
+            stackView.addArrangedSubview(rect)
+        }
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        scrollView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+        ])
+        scrollView.contentSize = CGSize(width: view.frame.width, height: (rectHeight * CGFloat(numOfBlocks)) + 64)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createRectangle() -> UIView {
+        let view = UIView()
+        
+        NSLayoutConstraint.activate([
+            view.heightAnchor.constraint(equalToConstant:200),
+            view.widthAnchor.constraint(equalToConstant:400)
+        ])
+        view.layer.cornerRadius = 15
+        view.backgroundColor = .systemPink
+        return view
     }
-    */
-
+    
 }
