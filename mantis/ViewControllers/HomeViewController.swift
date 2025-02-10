fb8247616de132f2633b7e11362191b035cd083f
llowe11 <vladislavkim566@gmail.com>
2025-02-10 20:31:50 +0500

    FEAT: create ui scroll view

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private var scrollView: UIScrollView()
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
 
