b9211d832387c5e7b7105636ffa318029ca0ecdf
llowe11 <vladislavkim566@gmail.com>
2025-02-15 18:53:03 +0500

    FEAT: create home page scrollable menu

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -1,11 +1,53 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private var scrollView = UIScrollView()
-    
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let stackView = UIStackView()
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
+        
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
 
+        
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stackView)
+        
+        for i in 1...20 {
+                    let label = UILabel()
+                    label.text = "Элемент \(i)"
+                    label.textAlignment = .center
+                    label.backgroundColor = .systemGray5
+                    label.heightAnchor.constraint(equalToConstant: 50).isActive = true
+                    stackView.addArrangedSubview(label)
+        }
+        NSLayoutConstraint.activate([
+                    scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+                    scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                    
+                    contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                    contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+                    contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                    contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                    contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                    
+                    stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+                    stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+                    stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+                    stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
+                ])
     }
     
 }
