8ecb2af1de784a3bb71fd8d990f484f775a72ec0
llowe11 <vladislavkim566@gmail.com>
2025-02-23 18:45:27 +0500

    FEAT: edit homeview

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -7,7 +7,40 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+        
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 16
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.bottomAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor)
+        ])
+        
+        let label: UILabel = {
+            let lbl = UILabel()
+            lbl.text = "Hello world"
+            lbl.textColor = .black
+            lbl.font = UIFont.systemFont(ofSize: 16)
+            return lbl
+        }()
+        
+        stackView.addArrangedSubview(label)
     }
     
 }
