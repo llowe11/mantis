80ee3a113f910f3116a5bb25ad83a513566ac28c
llowe11 <vladislavkim566@gmail.com>
2025-02-27 19:55:32 +0500

    FEAT: edit home view controller

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
