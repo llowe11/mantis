30d1cd26410829192417b6cf0db0d134275aba1a
llowe11 <vladislavkim566@gmail.com>
2025-02-27 19:53:43 +0500

    FEAT: edit home view controller

--- mantis/ViewControllers/HomeViewController.swift
+++ mantis/ViewControllers/HomeViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
-        let button: UIButton = {
-            let btn = UIButton()
-            
-            
-            return btn
-        }()
         
+        setupConstraints()
     }
-    private func setupLayout() {
-        
-    }
-    private func configureContent() {
-        
-    }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
 
+        ])
+    }
 }
+
